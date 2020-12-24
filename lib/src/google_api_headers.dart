dc10e70e8c974b2ca35c3850cfd6fd0ec1d8d88a
zeshuaro <joshuaystang@gmail.com>
2020-12-25 02:16:36 +1100

    Update/docs (#1)
    * Update readme
    
    * Add changelog
    
    * Update static member
    
    * Update readme
    
    * Add comments

--- lib/src/google_api_headers.dart
+++ lib/src/google_api_headers.dart
@@ -2,16 +2,28 @@ import 'package:google_api_headers/src/my_platform.dart';
 import 'package:package_info/package_info.dart';
 import 'package:flutter/services.dart';
 
+/// GoogleApiHeaders provide a method for getting the headers required for
+/// calling Google APIs with a restricted key.
+///
+/// ```dart
+/// import 'package/google_api_headers/google_api_headers.dart';
+///
+/// final headers = await GoogleApiHeaders().getHeaders();
+/// ```
 class GoogleApiHeaders {
   final MyPlatform platform;
 
-  GoogleApiHeaders([MyPlatform platform])
-      : platform = platform ?? MyPlatformImp();
+  /// Constructor with an optional parameter of [MyPlatform].
+  /// Default to [MyPlatformImp] if the parameter is not provided.
+  const GoogleApiHeaders([MyPlatform platform])
+      : platform = platform ?? const MyPlatformImp();
 
+  static Map<String, String> _headers = {};
   final MethodChannel _channel = const MethodChannel('google_api_headers');
-  Map<String, String> _headers = {};
 
-  Future<Map<String, dynamic>> getHeaders() async {
+  /// Get the headers required for calling Google APIs with a restricted key
+  /// based on the platform.
+  Future<Map<String, String>> getHeaders() async {
     if (_headers.isEmpty) {
       final packageInfo = await PackageInfo.fromPlatform();
       if (platform.isIos()) {
--- lib/src/my_platform.dart
+++ lib/src/my_platform.dart
@@ -1,11 +1,22 @@
 import 'dart:io';
 
+/// Abstract class for platform implementations.
 abstract class MyPlatform {
+  /// Abstract const constructor.
+  const MyPlatform();
+
+  /// Returns whether the platform is Android or not.
   bool isAndroid();
+
+  /// Returns whether the platform is iOS or not.
   bool isIos();
 }
 
+/// MyPlatformImp implements the methods of whether the platform is
+/// iOS or Android.
 class MyPlatformImp implements MyPlatform {
+  const MyPlatformImp();
+
   @override
   bool isAndroid() => Platform.isAndroid;
 
