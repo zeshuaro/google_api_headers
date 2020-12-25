f2d1d42ae5a2fa4bf0b3fa57d1a23085d2fd957d
zeshuaro <joshuaystang@gmail.com>
2020-12-25 12:26:14 +1100

    Add example app (#5)

---
+++ example/lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_api_headers/google_api_headers.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Map<String, dynamic> _headers;
+
+  @override
+  void initState() {
+    super.initState();
+    getHeaders();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text(
+            'Google API headers: ${_headers.toString()}',
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> getHeaders() async {
+    final headers = await GoogleApiHeaders().getHeaders();
+    setState(() => _headers = headers);
+  }
+}
